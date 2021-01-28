a692385d3c800da3090ec9d6ed7c26a765eff1ca
devnafiz <nafiz.2011@yahoo.com>
2021-01-29 00:13:11 +0600

    update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,9 +6,16 @@ use App\Product;
 use Illuminate\Http\Request;
 use App\Http\Resources\Product\ProductResource;
 use App\Http\Resources\Product\ProductCollection;
+use App\Http\Requests\ProductRequest;
 
 class ProductController extends Controller
 {
+    
+    public function __construct(){
+
+        $this->middleware('auth:api')->except('index','show');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -35,9 +42,24 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+
+        $product=new Product;
+        $product->name=$request->name;
+         $product->detail=$request->description;
+         $product->price=$request->price;
+         $product->stock=$request->stock;
+         $product->discount=$request->discount;
+         $product->save();
+       return response([
+           'data'=>new ProductResource($product)
+
+
+       ],201);
+
+
+        //return $request->all();
     }
 
     /**
@@ -73,7 +95,15 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        //return $request->all();
+        $request['detail']=$request->description;
+        unset($request['description']);
+        $product->update($request->all());
+        return response([
+           'data'=>new ProductResource($product)
+
+
+       ],Response::HTT_CREATED);
     }
 
     /**
